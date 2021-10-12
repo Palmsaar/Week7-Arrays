2dd62a78047e604f80c72aa7553d4914b79140f2
unknown <apalmsaar@gmail.com>
2021-10-12 11:10:41 +0300

    add array

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] predictions = new string[4] {"win a million", "fall in love", "buy youtube premium", "join the darkside for cookies" };
+            Console.WriteLine("enter a number form 0 to 3");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Today you will {predictions[Num]}");
+        }
+    }
+}
